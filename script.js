a9e88c9f0373624340fd0cd2e667d7c1d512361e
ReneBoot2025 <reneraul123@gmail.com>
2025-03-30 15:27:12 -0500

    init commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth Scroll para los enlaces de navegación
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const section = document.querySelector(this.getAttribute('href'));
+        section.scrollIntoView({ behavior: 'smooth' });
+    });
+});
